4440f25aa147c406c74485608bc08b64971032c7
akhand08 <adib08@outlook.com>
2025-09-29 15:37:16 +0600

    Implemented basic continuous json streaming from STDIN

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type RequestBody struct {
+	Type      string `json:"type"`
+	MessageID int    `json:"msg_id"`
+	InReplyTo int    `json:"in_reply_to"`
+}
+
+type RequestNode struct {
+	Source      string `json:"src"`
+	Destination string `json:"dest"`
+	Body        RequestBody
+}
 
 func main() {
-	fmt.Println("This is my new Go project!")
+
+	reader := bufio.NewReader(os.Stdin)
+	decoder := json.NewDecoder(reader)
+
+	fmt.Fprintln(os.Stderr, "Server is started to running")
+
+	for decoder.More() {
+
+		var request RequestNode
+		err := decoder.Decode(&request)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error decoding JSON message: %v\n", err)
+			continue
+		}
+
+		fmt.Println("Response: ")
+		fmt.Println("-------------------------------------------------------")
+		fmt.Println(request)
+		fmt.Println("-------------------------------------------------------")
+
+	}
+
 }
